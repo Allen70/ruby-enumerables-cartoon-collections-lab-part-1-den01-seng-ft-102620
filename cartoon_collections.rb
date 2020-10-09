fad92a9848e006743e3a65ee2377f59d911cb51e
Allen Richard Lea <allenlea2007@gmail.com>
2020-10-09 21:10:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,9 +12,10 @@ def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
-  
+  count = 0
   array.each_with_index do |element|
     name = element
+    count += 1
     puts "1. #{element}"
   end
 end
